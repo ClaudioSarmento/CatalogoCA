958e07dc640f884b5c3dba074454039382206e8a
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-06-06 10:29:58 -0300

    feature - configuração Infrastructure

---
+++ CatalogoCA.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CatalogoCA.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoCA.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext)
+                .Assembly);
+        }
+    }
+}
